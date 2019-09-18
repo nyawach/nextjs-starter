2fe44cbd26aa57715b63908ac740efa2fca56d23
himeno-yusuke <himeno-yusuke@kayac.com>
2019-09-18 16:46:02 +0900

    共通ヘッダー/フッターのサンプル

--- src/layouts/Default.tsx
+++ src/layouts/Default.tsx
@@ -1,5 +1,7 @@
 import React from "react"
 import Head from "next/head"
+import Header from "~/components/Header"
+import Footer from "~/components/Footer"
 
 const Default = ({ children }) => {
   return (
@@ -9,9 +11,9 @@ const Default = ({ children }) => {
         <meta charSet="utf-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      <header>header</header>
+      <Header />
       {children}
-      <footer>footer</footer>
+      <Footer />
     </>
   )
 }
